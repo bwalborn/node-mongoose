8d9b6269e6a911a04015165242a97f4be8f6db5f
bwalborn <walbornbryce@gmail.com>
2020-11-02 15:12:03 -0600

    Mongoose Part 2

--- index.js
+++ index.js
@@ -3,20 +3,18 @@ const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
-const connect = mongoose.connect(url, { useUnifiedTopology: true , useNewUrlParser: true });
+const connect = mongoose.connect(url, { useCreateIndex: true , useUnifiedTopology: true , useNewUrlParser: true });
 
 connect.then((db) => {
     console.log('Connected correctly to server');
-    var newDish = Dishes({
+    var newDish = Dishes.create({
         name: 'Uthappizza',
         description: 'test'
-    });
-
-    newDish.save()
+    })
         .then((dish) => {
             console.log(dish);
 
-            Dishes.find({}).exec();
+           return Dishes.find({}).exec();
         })
         .then((dishes) => {
             console.log(dishes);
